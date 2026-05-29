207f70df7d6e8fbb7756ced8c36b3de1f08374b2
Ankit Katkhede <ankitkatkhede@gmail.com>
2026-05-29 23:19:43 +0530

    Add new Code

---
+++ String_Progarm/src/string/com/PrintDuplicate.java
@@ -0,0 +1,20 @@
+package string.com;
+
+public class PrintDuplicate {
+
+	public static void main(String[] args) {
+	char ch[]= {'a','b','c','b','e','f','g','a'};
+	for(int i=0;i<ch.length;i++)
+	{
+		for(int j=i+1;j<ch.length;j++)
+		{
+			if(ch[i]==ch[j])
+			{
+				System.out.print(ch[i]+" ");
+			}
+		}
+	}
+
+	}
+
+}
